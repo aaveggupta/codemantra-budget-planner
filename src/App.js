42276d862f67447f1866eec3ccbf7d04dd2948ce
Aaveg Gupta <aaveggupta.contact@gmail.com>
2022-10-29 13:17:54 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import "./App.css";
 import HomePage from "./Pages/HomePage/HomePage";
 
 const App = () => {
-  // useState(() => {
-  //   const url = new URL(
-  //     "https://www.googletagmanager.com/gtm.js?id=GTM-NJGNR68"
-  //   );
-  //   const proxyUrl = new URL(window.location.hostname);
-  //   proxyUrl.searchParams.append("url", url.href);
-  //   console.log(url.href);
-  //   console.log(proxyUrl);
-  // }, []);
+  useEffect(() => {
+    console.log(window.location);
+  }, []);
 
   return (
     <div>
